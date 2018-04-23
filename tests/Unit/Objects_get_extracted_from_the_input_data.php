a5bc8fb0860107b304d877c1363f8ebf1ab830ab
Stratadox <info@stratadox.com>
2018-04-23 08:45:19 +0200

    Move tests to common ground

--- tests/Unit/Objects_get_extracted_from_the_input_data.php
+++ tests/Unit/Objects_get_extracted_from_the_input_data.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Stratadox\TableLoader\Test\Unit\MakesObjects;
+namespace Stratadox\TableLoader\Test\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Stratadox\Hydrator\MappedHydrator;
