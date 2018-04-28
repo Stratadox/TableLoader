c63746553d143637bef3bb14f543172b8f7cfe22
Stratadox <info@stratadox.com>
2018-04-28 17:29:16 +0200

    Update table loader interface from method docblock

--- contracts/LoadsTables.php
+++ contracts/LoadsTables.php
@@ -15,7 +15,7 @@ interface LoadsTables
     /**
      * Transforms table data into an object collection.
      *
-     * @param string[][] $input A list of associative arrays.
+     * @param array[]    $input A list of associative arrays.
      * @param Map|null   $map   The map that contains entities by id.
      * @return ResultingObjects The collection of objects as
      *                          [label => [id => object]].
