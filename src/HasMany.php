e4ce7d361e2de02dc5f9efe5e6603ec17ae8236b
Stratadox <info@stratadox.com>
2018-04-28 15:28:57 +0200

    Skip adding relations that are already introduced

--- src/HasMany.php
+++ src/HasMany.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Stratadox\TableLoader;
 
+use function in_array;
 use Stratadox\Hydrator\ArrayHydrator;
 use Stratadox\Hydrator\CannotHydrate;
 use Stratadox\Hydrator\Hydrates;
@@ -43,13 +44,31 @@ final class HasMany implements MakesConnections
         KnowsWhereToLook $to,
         ContainsResultingObjects $objects
     ): array {
-        // @todo add caching?
         $related = [];
         foreach ($data as $row) {
-            // @todo skip if already there?
-            $related[$from->this($row)][] = $objects[$to->label()][$to->this($row)];
+            $object = $objects[$to->label()][$to->this($row)];
+            if ($this->shouldAdd($object, $from->this($row), $related)) {
+                $related[$from->this($row)][] = $object;
+            }
         }
         return [$this->property => $this->makeCollections($from->label(), $related)];
+    }
+
+    /**
+     * Checks if we should add the object to the relations collection.
+     *
+     * @param object $relation The related object.
+     * @param string $from     The id of the object that holds the relationship.
+     * @param array  $existing The map of existing relationships.
+     * @return bool            Whether the object is new.
+     */
+    private function shouldAdd(
+        object $relation,
+        string $from,
+        array $existing
+    ): bool {
+        return !isset($existing[$from])
+            || !in_array($relation, $existing[$from], true);
     }
 
     /**
