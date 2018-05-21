dc054b6afe8f1bfa929db268a23ef449f8d9c31b
Stratadox <info@stratadox.com>
2018-05-21 16:53:48 +0200

    Add missing value objects to identity map ignore list

--- tests/Integration/Shop/Catalogue/Infrastructure/MappedLoader.php
+++ tests/Integration/Shop/Catalogue/Infrastructure/MappedLoader.php
@@ -10,8 +10,10 @@ use Stratadox\TableLoader\LoadsTables;
 use Stratadox\TableLoader\Test\Integration\Shop\Catalogue\Domain\Price\Money;
 use Stratadox\TableLoader\Test\Integration\Shop\Catalogue\Domain\Reason\Feature;
 use Stratadox\TableLoader\Test\Integration\Shop\Catalogue\Domain\Reason\NumberAttribute;
+use Stratadox\TableLoader\Test\Integration\Shop\Catalogue\Domain\Reason\NumberValue;
 use Stratadox\TableLoader\Test\Integration\Shop\Catalogue\Domain\Reason\TextAttribute;
 use Stratadox\TableLoader\Test\Integration\Shop\Catalogue\Domain\Reason\TextListAttribute;
+use Stratadox\TableLoader\Test\Integration\Shop\Catalogue\Domain\Reason\TextValue;
 
 final class MappedLoader implements LoadsTables
 {
@@ -43,7 +45,9 @@ final class MappedLoader implements LoadsTables
             Feature::class,
             NumberAttribute::class,
             TextAttribute::class,
-            TextListAttribute::class
+            TextListAttribute::class,
+            NumberValue::class,
+            TextValue::class
         );
 
         $result = $make->from($input, $map);
